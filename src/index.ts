af5af2780b5131c46e47288e26b9927e099b59ff
harislapo <haris.lapo@gmail.com>
2022-12-22 18:08:32 +0100

    Added mongoose, model, .env

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
-import express from 'express';
+import { config } from 'dotenv';
+import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import Deck from './models/Deck';
 
+config();
+
+const PORT = 5000;
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello!!');
+app.post('/decks', async (req: Request, res: Response) => {
+  const deck = new Deck({
+    title: req.body.title,
+  });
+  const response = await deck.save();
+  res.json(response);
 });
 
-app.listen(5000);
+const db = mongoose.connect(process.env.MONGO_URL!).then(() => {
+  console.log(`Listening on port ${PORT}`);
+  app.listen(PORT);
+});
